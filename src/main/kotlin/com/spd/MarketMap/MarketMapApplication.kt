3ae98600b6b0c3a959bcf7981fe10d87ad64e998
pr0p1k <propiktunes@gmail.com>
2019-05-25 00:18:46 +0300

    Initialized

---
+++ src/main/kotlin/com/spd/MarketMap/MarketMapApplication.kt
@@ -0,0 +1,11 @@
+package com.spd.MarketMap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketMapApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarketMapApplication>(*args)
+}
---
+++ src/main/kotlin/com/spd/MarketMap/entities/PlaceMap.kt
@@ -0,0 +1,9 @@
+package com.spd.MarketMap.entities
+
+import java.io.Serializable
+import javax.persistence.Entity
+
+@Entity
+data class PlaceMap(val name: String) : Serializable {
+    // TODO
+}
